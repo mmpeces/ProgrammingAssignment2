048fdd78cbd5b855ce8e9084bd9e2e8de949828f
mmpeces <mmpeces@gmail.com>
2014-08-17 15:05:35 -0700

    master

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
+##cacheSolve function computes the inverse of the special "matrix" returned by 
+##makeCacheMatrix above. If the inverse has already been calculated 
+##(and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
 
-## Write a short comment describing this function
+
+
+## makeCacheMatrix is a list containing a funtion to:
+##1. sets the value of the matrix
+##2. gets the value of the matrix
+##3. sets the value og the inverse matrix
+##4. gets the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve function calculates the inverse matrix of the special "matrix" created with makeCacheMatrix. 
+##However, it first checks to see if the inverse matrix has already been calculated. If so, it gets the 
+##inverse matrix from the cache and skips the computation. Otherwise, it calculates the inverse matrix
+##of the data and sets the value of the inverse matrix in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
+## Return a matrix that is the inverse of 'x'
