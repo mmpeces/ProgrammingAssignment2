f9bd2f2c0bf612ed20c483fc7bd302a3c95eae72
mmpeces <mmpeces@gmail.com>
2014-08-18 15:47:54 -0700

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,6 @@
-## makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
+##Notes are paraphrased from Programming Assignment 2 exampl of Caching the Mean of a Vector.  See
+##https://class.coursera.org/rprog-006/human_grading/view/courses/972578/assessments/3/submissions
+##makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
 ##cacheSolve function computes the inverse of the special "matrix" returned by 
 ##makeCacheMatrix above. If the inverse has already been calculated 
 ##(and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
